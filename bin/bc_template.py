5c291ca65f3513d4835c38d247e776a6ad3830a3
Sandie Peters <sandiepeters@comast.net>
2025-06-19 00:26:50 -0700

    Added logic for generating adapter sequences surrounding the genome tag to be used in cutadapt, #32

--- bin/bc_template.py
+++ bin/bc_template.py
@@ -3,13 +3,15 @@
 from Bio import SeqIO
 import sys
 import re
+from snapgene_reader import snapgene_file_to_seqrecord
 
-def extract_flanks(path, out_type, trim_insert_flank=False):
+def extract_flanks(path, out_type, trim_insert_flank=False,  construct=None):
     """
     Extracts flanking sequences from a genbank file and returns them in the format required by cutadapt
     :param path: Path to the genbank file containing the flanking sequences
     :param out_type: string representing the annotation type to be extracted. Either 'cutadapt_barcode' or 'cutadapt_insert'
-    :param trim_insert_flank: boolean indicating whether any trimming should occur for flanking insert sequences
+        :param trim_insert_flank: boolean indicating whether any trimming should occur for flanking insert sequences
+    :param construct: string representing the construct name, used for bartender output
     :return: string representing the flanking sequences in the format required by cutadapt
     """
 
@@ -26,9 +28,39 @@ def extract_flanks(path, out_type, trim_insert_flank=False):
         seq_up = str(ordered_seqs[0].seq)
         seq_dn = str(ordered_seqs[1].seq)
         if trim_insert_flank:
+
             seq_up = trim_string(seq_up, 0, 35)
             seq_dn = trim_string(seq_dn, 35, 0)
         bc = f'{seq_up}...{seq_dn}'
+    elif out_type == "cutadapt_genome_tag":
+        ordered_seqs = order_based_on_position(filter_seqs_on_name(seqs, r'Barcode_(R|F)|INSERT(UP|DN)|Secondary_Barcode_for_Donor_gDNA'))
+        if len(ordered_seqs) != 5:
+            raise ValueError("Not enough sequences found for cutadapt_genome_tag. Expected at least 5 sequences, found {}".format(len(ordered_seqs)))
+        # We're assuming when ordered, the 2nd and 3rd are the INSERTDN and Secondary Barcode for Donor gDNA sequences
+
+        # ---<INSERTUP>---<INSERTDN---<Secondary Barcode for Donor gDNA>-->---<Barcode F>---<Barcode R>---
+
+        # We care about the span between INSERTDN and Secondary Barcode for Donor gDNA and the span between
+        # the end of the Secondary Barcode for Donor gDNA and the start of Barcode F
+        #                 <--<INSERTDN>---<Secondary Barcode for Donor gDNA>-->---<Barcode F>
+
+        # The first adapter is formed by truncating the insert dn sequence to the start of the gDNA tag sequence
+        # The second adapter is formed by truncating the end of gDNA tag to the start of the Barcode F sequence
+
+        insert_dn = ordered_seqs[1]
+        g_tag = ordered_seqs[2]
+        barcode_f = ordered_seqs[3]
+        insert_up_start = insert_dn.features[0].location.start
+        g_tag_start = g_tag.features[0].location.start
+        g_tag_end = g_tag.features[0].location.end
+        barcode_f_start = barcode_f.features[0].location.start
+        adapter_1 = str(insert_dn.seq[:g_tag_start - insert_up_start])
+        intervening_seq = get_intervening_sequence(construct, g_tag_end, barcode_f_start)
+
+        adapter_2 = str(intervening_seq)
+
+        bc = f'{adapter_1}...{adapter_2}'
+
     elif out_type == "bartender":
         ordered_seqs = order_based_on_position(filter_seqs_on_name(seqs, r'BARCODE[0-9]{0,2}(UP|DN)'))
         bc = f'{str(ordered_seqs[0].seq[-5:])}[20]{mid}[20]{str(ordered_seqs[1].seq[:5])}'
@@ -74,9 +106,23 @@ def trim_string(input_string, start_trim, end_trim):
     return input_string[start_trim:len(input_string) - end_trim]
 
 
+def get_intervening_sequence(construct, start, end):
+    """
+    Extracts the intervening sequence between two positions in a sequence.
+
+    :param construct: The path to the SnapGene file
+    :param start: Start position (0-based)
+    :param end: End position (0-based)
+    :return: The intervening sequence as a string
+    """
+    seq = snapgene_file_to_seqrecord(construct)
+    return seq[start:end].seq
 
 if __name__ == "__main__":
+
     if len(sys.argv) == 3:
         extract_flanks(sys.argv[1], sys.argv[2])
     elif len(sys.argv) == 4:
         extract_flanks(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif len(sys.argv) == 5:
+        extract_flanks(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
--- bin/get_flanking.py
+++ bin/get_flanking.py
@@ -8,14 +8,15 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation
 import sys
 import re
 
+
 def extract_flanks(path):
     """
-    Parse the SnapGene file and extract the flanking sequences based on the provided regex
+    Parse the SnapGene file and extract the features matching the given patterns.
     :param path: Path to the SnapGene file
     :return: Written Genbank file with the flanking sequences
     """
     con = snapgene_file_to_dict(path)
-    part_labels = [r'INSERT(UP|DN)', r'BARCODE[0-9]{0,2}(UP|DN)']
+    part_labels = [r'INSERT(UP|DN)', r'BARCODE[0-9]{0,2}(UP|DN)',  r'Barcode (R|F)', 'Secondary Barcode for Donor gDNA']
     part_list = {}
     for x in con['features']:
         if any(re.match(reg, x['name']) for reg in part_labels):
@@ -24,17 +25,18 @@ def extract_flanks(path):
     flanking_seqs = []
     for x,y in part_list.items():
         seq = con['seq'][y[0]:y[1]]
-        # We need location information later on to correctly use annotations in cutadapt
+        # We need location information later on to correctly orient adapters in cutadapt
         f = FeatureLocation(y[0], y[1])
         feature = SeqFeature(f, type = "misc_feature")
-        record = SeqRecord(Seq(seq), id = x, description = "", features=[feature], annotations={"molecule_type": "DNA"})
+        # Locus names are not allowed to have spaces, so we replace them with underscores
+        record = SeqRecord(Seq(seq), id = x.replace(" ","_"), description = "", features=[feature], annotations={"molecule_type": "DNA"})
         flanking_seqs.append(record)
 
     return flanking_seqs
 
 
-
-construct_file = sys.argv[1]
-flanks = extract_flanks(construct_file)
-# Write Genbank format to preserve location information
-SeqIO.write(flanks, "flanking.gb", "genbank")
+if __name__ == "__main__":
+    construct_file = sys.argv[1]
+    flanks = extract_flanks(construct_file)
+    # Write Genbank format to preserve location information
+    SeqIO.write(flanks, "flanking.gb", "genbank")
